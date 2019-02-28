346a5d1a5439c397cd5ae5ed2bb3498c83704232
Justin Wu <justinhwu95@gmail.com>
2019-02-28 18:31:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  kittens.slice(2, kittens.length - 1)
-  return kittens
+  return kittens.slice(2, kittens.length - 1)
+  /*return kittens*/
 }
